44c2f290ff7d34c267078fbee596e8262d0c11ca
patomation <patomation@gmail.com>
2020-09-29 12:49:15 +0700

    feat(keyword): adds some more keywords

--- src/bot/keywords.ts
+++ src/bot/keywords.ts
@@ -2,5 +2,21 @@ export const keywords = {
   PJSalt: {
     say: 'Salty Salty Salty Salty Salty Salty Salty Salty Salty Salty Salty Salty',
     text: 'PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt PJSalt'
+  },
+  python: {
+    text: 'python is ok. Whats up with all the tabs. Where did the brackets go?'
+  },
+  LUL: {
+    say: 'ha ha ha he he he ho ho ho',
+    text: 'LUL LUL LUL LUL LUL LUL LUL'
+  },
+  coffee: {
+    text: 'damn good coffee is always damn good'
+  },
+  hi: {
+    text: 'sup'
+  },
+  hello: {
+    text: 'hey there'
   }
 }
