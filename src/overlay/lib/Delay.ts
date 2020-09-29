1561b91ff083e6933be7061c14a347e037f06410
patomation <patomation@gmail.com>
2020-09-29 13:38:42 +0700

    feat(overlay): delay module tracked

---
+++ src/overlay/lib/Delay.ts
@@ -0,0 +1,27 @@
+// WARNING: timer is shared with every instance could cause bugs
+// Make a new instance for a different timer
+
+export type Callback = (...args: (string | boolean | number | number[] | Record<string, unknown>)[]) => void
+
+export default class Delay {
+  constructor (callback?: Callback, ms?: number) {
+    if (callback) {
+      this.start(callback, ms)
+    }
+  }
+
+  timer = 0
+  /**
+ * A callback Delay function
+ * @param callback Callback method
+ * @param ms delay time
+ */
+  start (callback: Callback, ms = 1000): void {
+    this.cancel()
+    this.timer = setTimeout(callback, ms)
+  }
+
+  cancel (): void {
+    clearTimeout(this.timer)
+  }
+}
