ecb15142c87a5b71c5fe3ad0d4d9877fc2ad2851
patomation <patomation@gmail.com>
2020-10-10 14:22:10 +0700

    feat(keyword): adds some more keywords

--- src/bot/keywords.ts
+++ src/bot/keywords.ts
@@ -19,7 +19,23 @@ export const keywords: Commands = {
     say: 'Shutting Down'
   },
   python: {
-    text: 'python is ok. Whats up with all the tabs. Where did the brackets go?'
+    text: [
+      'python is ok. Whats up with all the tabs. Where did the brackets go?',
+      'python is that a snek?'
+    ]
+  },
+  javascript: {
+    text: [
+      'Is that the same as java',
+      'I like coffee and scrolls',
+      'And the bartender says, Success, but youre not ready!. So a JavaScript function walks into a bar.',
+      'Why was the javascript developer sad? Because he didnt Node how to Express himself.'
+    ]
+  },
+  rust: {
+    text: [
+      ' break out the WD40'
+    ]
   },
   LUL: {
     say: 'ha ha ha he he he ho ho ho',
@@ -37,12 +53,23 @@ export const keywords: Commands = {
       'welcome to the stream'
     ]
   },
+  moobot: {
+    text: [
+      'moooooooooooooooooooooooooooooooo'
+    ]
+  },
   nightbot: {
     text: [
       '@nightbot? more like daybot! LUL',
       '@nightbot smells of cheese',
       '@nightbot is like a box of chocolates...',
       '@nightbot, why u do dis'
+    ]
+  },
+  boring: {
+    text: [
+      'youre booring',
+      'no you'
     ]
   }
 }
