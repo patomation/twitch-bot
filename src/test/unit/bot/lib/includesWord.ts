83bd8b51d37907d60682d9851e28655c6f9af20f
patomation <patomation@gmail.com>
2020-10-18 22:51:28 +0700

    feat(keyword): tests message includes word module
    fixes #28

---
+++ src/test/unit/bot/lib/includesWord.ts
@@ -0,0 +1,38 @@
+import test from 'ava'
+import { includesWord } from '../../../../bot/lib/includesWord'
+
+const data = [
+  {
+    word: 'hi',
+    text: 'hi',
+    expected: true
+  },
+  {
+    word: 'hi',
+    text: ' hi',
+    expected: true
+  },
+  {
+    word: 'hi',
+    text: 'hi! there',
+    expected: true
+  },
+  {
+    word: 'hi',
+    text: 'hi friend',
+    expected: true
+  },
+  {
+    word: 'hi',
+    text: 'this',
+    expected: false
+  }
+]
+
+test('Sentence Includes words', (t) => {
+  t.plan(data.length)
+  data.forEach(({ text, word, expected }) => {
+    const actual = includesWord(text, word)
+    t.is(actual, expected)
+  })
+})
