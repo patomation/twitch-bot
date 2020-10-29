4c2c98fdc74c22ea820e1c841147640210ca2bbe
patomation <patomation@gmail.com>
2020-10-29 11:05:10 +0700

    feat(chat-log-reader): waits 2 sec and reads next added line in a cue

--- scripts/display-chat-log.js
+++ scripts/display-chat-log.js
@@ -4,34 +4,45 @@ const readline = require('readline')
 
 const chatLogPath = path.resolve(__dirname, '../', './logs/chat_log.txt')
 
-const readLastLine = (filePath) => new Promise((resolve) => {
-  let lastLine = ''
+const readLines = (filePath) => new Promise((resolve) => {
+  const lines = []
   if (fs.existsSync(filePath)) {
     const fileStream = fs.createReadStream(filePath)
     const lineReader = readline.createInterface({
       input: fileStream
     })
     lineReader.on('line', (line) => {
-      lastLine = line
+      lines.push(line)
     })
-
     lineReader.on('close', () => {
-      resolve(lastLine)
+      resolve(lines)
     })
   }
 })
 
-let lastLine = ''
-const displayLastLine = async () => {
-  const nextLastLine = await readLastLine(chatLogPath)
-  if (lastLine !== nextLastLine) {
-    lastLine = nextLastLine
-    // logs to terminal without last line - thank twitch user @jordicat
-    process.stdout.write(`\n${lastLine}`)
+let displayLine = ''
+let lastLineIndex = null
+const displayNextLine = async () => {
+  const lines = await readLines(chatLogPath)
+  const nextLine = lines.slice(lastLineIndex + 1, lines.length)[0]
+
+  // Handle if new lines
+  if (nextLine) {
+    lastLineIndex++
+    displayLine = nextLine
   }
+  process.stdout.write(`\n${displayLine}`)
+
   setTimeout(() => {
-    displayLastLine()
-  }, 500)
+    displayNextLine()
+  }, 2000)
 }
 
-displayLastLine()
+const main = async () => {
+  const lines = await readLines(chatLogPath)
+  lastLineIndex = lines.length - 1
+  displayLine = lines[lastLineIndex]
+  console.log({ lines, lastLine: displayLine })
+  displayNextLine()
+}
+main()
