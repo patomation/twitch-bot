426fc48fd0565f6dbebddc0fe6fa6092bf1b6b71
patrick <patrick@colosus>
2020-10-30 12:43:41 +0700

    fix(command-marque): improves marque and removes stutter problem

--- src/overlay/routes/commands-marque.ts
+++ src/overlay/routes/commands-marque.ts
@@ -2,53 +2,90 @@ import { h } from 'snabbdom/build/package/h'
 import { VNode } from 'snabbdom/build/package/vnode'
 import { render } from '../lib/render'
 import { host } from '../host'
+import { VNodeStyle } from 'snabbdom/build/package/modules/style'
 
-const marqueCss = `
-.marquee {
-  position: relative;
-  overflow: hidden;
-  --offset: 20vw;
-  --move-initial: calc(-25% + var(--offset));
-  --move-final: calc(-50% + var(--offset));
+let state = {
+  init: true,
+  soundCommands: [],
+  showFirst: true,
+  showSecond: false,
+  containerWidth: 1000, // needs to have some sort of default starting width
+  contentWidth: 3000 // needs to have some sort of default starting width
 }
 
-.marquee__inner {
-  width: fit-content;
-  display: flex;
-  position: relative;
-  transform: translate3d(var(--move-initial), 0, 0);
-  animation: marquee 20s linear infinite;
-  animation-play-state: running;
+const handleFirstTransitionEnd = () => {
+  state.showFirst = false
+  state.showSecond = true
+  render(view(state))
 }
 
-.marquee span {
-  font-size: 2.5rem;
-  padding: 0 2vw;
+const handleSecondTransitionEnd = () => {
+  state.showSecond = false
+  state.showFirst = true
+  render(view(state))
 }
 
-@keyframes marquee {
-  0% {
-      transform: translate3d(var(--move-initial), 0, 0);
-  }
-
-  100% {
-      transform: translate3d(var(--move-final), 0, 0);
-  }
+const handleContainerInsert = (vnode: VNode): void => {
+  state.containerWidth = (vnode.elm as HTMLElement).offsetWidth
 }
-`
 
-const view = (soundCommands: string[]): VNode =>
+const handleContentInsert = (vnode: VNode): void => {
+  state.contentWidth = (vnode.elm as HTMLElement).offsetWidth
+}
+
+const speed = 100
+
+const view = ({ soundCommands, showFirst, showSecond, containerWidth, contentWidth }: typeof state): VNode =>
   h('div.marquee', {
+    hook: { insert: handleContainerInsert },
     style: {
-      color: '#ffffff'
+      color: '#ffffff',
+      fontSize: '2.5rem'
     }
   }, [
-    h('div.marquee__inner', soundCommands.map((command) => h('span', `!${command}`))),
-    h('style', marqueCss)
+    showFirst
+      ? h('span.marquee__content1', {
+        style: {
+          position: 'absolute',
+          right: `${-contentWidth}px`,
+          transition: `right linear ${contentWidth / speed}s`,
+          delayed: {
+            right: '0px'
+          },
+          remove: {
+            transition: `right linear ${containerWidth / speed}s`,
+            right: `${containerWidth}px`
+          }
+        } as unknown as VNodeStyle,
+        on: { transitionend: handleFirstTransitionEnd },
+        hook: { insert: handleContentInsert }
+      }, soundCommands.map((command) =>
+        h('span', { style: { padding: '0 1rem' } }, `!${command}`)))
+      : null,
+    showSecond
+      ? h('span.marquee__content2', {
+        style: {
+          position: 'absolute',
+          right: `-${contentWidth}px`,
+          transition: `right linear ${contentWidth / speed}s`,
+          delayed: {
+            right: '0px'
+          },
+          remove: {
+            transition: `right linear ${containerWidth / speed}s`,
+            right: `${containerWidth}px`
+          }
+        } as unknown as VNodeStyle,
+        on: { transitionend: handleSecondTransitionEnd },
+        hook: { insert: handleContentInsert }
+      }, soundCommands.map((command) =>
+        h('span', { style: { padding: '0 1rem' } }, `!${command}`)))
+      : null
   ])
 
 export const route = async (): Promise<void> => {
   const response = await fetch(`${host}/get-sound-commands`)
   const { soundCommands } = await response.json()
-  render(view(soundCommands))
+  state = { ...state, soundCommands }
+  render(view(state))
 }
